19e95c8f9b2879020133b2e1508121eef15629ab
Rommel Rodriguez Perez <alrodperez@gmail.com>
2022-06-04 19:33:08 -0500

    dded ilutils and updated bldkern, this time for real ...

---
+++ ilsutils.sh
@@ -0,0 +1,13 @@
+
+confirm(){
+	msg=$1
+	rsp=""
+	printf "%s \n(Y/n): " "${msg}"
+	read -r -e rsp # -r option to prevent \ to scape characters
+	rsp=$(echo "$rsp" | tr "[:upper:]" "[:lower:]")
+	if test "${rsp}" != "y"; then
+		echo "Aborting operation"
+		return 1 
+	fi
+	return 0 
+}
